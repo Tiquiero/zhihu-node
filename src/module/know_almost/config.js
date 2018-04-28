cf3d354e3ecadcc8d75a0e261dbefa0cccef096f
tiquiero <tiquiero@163.com>
2018-04-28 18:52:57 +0800

    first commit

---
+++ src/module/know_almost/config.js
@@ -0,0 +1,7 @@
+export default {
+    log: true,         /**日志模式*/
+    storage_path: './src/module/know_almost/images/',   /**存储路径*/
+    authorization: 'Bearer Mi4xaHNqWEFRQUFBQUFBSUt6am9hdzFEUmNBQUFCaEFsVk5uV1dDV3dDVUJSRm9jYVVXV2FiREZfWHZHMDM0b081Vzd3|1519720350|295ebdcd1530503e6caf61a5942fc1dba85ca363'
+    // authorization:"oauth c3cef7c66a1843f8b3a9e6a1e3160e20"
+    // authorization: 'Bearer Mi4xWGJIUkFnQUFBQUFBRUFLRkxBUzdDeGNBQUFCaEFsVk5JSlFiV3dBMUxmSE5mSmZiWGgyUEdTTnVrdmhuQWt1aEtB|1512982048|61da95df7ad7e07c46f4178364a720f31a0fb705'
+}
